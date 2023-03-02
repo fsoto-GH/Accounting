a20a3e5b975bebd30b45a0115f9ceedea7e17854
Fernando Soto <fsoto3@paylocity.com>
2023-03-02 12:53:44 -0600

    WIP triggers and TODO

--- DATABASE_SQL/TRIGGERS.sql
+++ DATABASE_SQL/TRIGGERS.sql
@@ -6,19 +6,23 @@ ON Accounts
 FOR UPDATE
 AS
 	DECLARE @newStatus bit
-	DECLARE @id int
+	DECLARE @id int, @debitTypeID int
 
 	SELECT @newStatus = i.Status, @id = i.AccountID
 	FROM inserted i
 
+	SELECT @debitTypeID = TransactionTypeID FROM TransactionTypes WHERE Name = 'DEBIT'
+
+	-- check that balance is 0 before closing account
 	IF @newStatus = 0 
 	BEGIN
 		DECLARE @netBalance decimal(18, 2)
 		SELECT
-			@netBalance = ISNULL(SUM(CASE WHEN T.Type = 'Debit' THEN -T.Amount ELSE T.Amount END), 0)
+			@netBalance = ISNULL(SUM(CASE WHEN T.TransactionTypeID = @debitTypeID THEN -T.Amount ELSE T.Amount END), 0)
 		FROM 
-			Accounts A INNER JOIN Transactions T
-			ON A.AccountID = T.AccountID
+			Accounts A INNER JOIN Transactions T ON A.AccountID = T.AccountID
+			INNER JOIN TransactionTypes TT ON T.TransactionTypeID = TT.TransactionTypeID
+
 		WHERE A.AccountID = @id
 
 		IF @netBalance != 0
@@ -27,3 +31,7 @@ AS
 			ROLLBACK
 		END
 	END
+
+	-- Trigger to delete a person.
+	-- Either execute the close account USP to ensure there is no balance or implement an alternative solution
+	-- UPDATE Referential integrity on Person FK to Cascade
